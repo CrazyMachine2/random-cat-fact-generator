c6a3f2ccc2460abcd83a3edf1d47a962883041c4
CrazyMachine2 <andres03@abv.bg>
2020-08-02 01:18:08 +0200

    cleaning js file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 const btn = document.getElementById('generator');
 const content = document.querySelector('.content');
-
-const catHolder = createElement('img', './img/cats-holder.png','cat-holder');
-const randomCat = createElement('img', '','random-cat');
-
-const imagesContainer = createElement('div', '','', 'images-container');
-imagesContainer.appendChild(catHolder);
-imagesContainer.appendChild(randomCat);
-
+const randomCat = document.getElementById('random-cat');
 const catFact = document.querySelector('.fact');
-
-console.log(catFact);
 
 btn.addEventListener('click', generateContent)
 
@@ -18,7 +9,6 @@ async function generateContent(event) {
     const randomImageUrl = await getRandomCatImageUrl();
     const randomFact = await getRandomCatFact();
     
-    const randomCat = document.getElementById('random-cat');
     randomCat.src = randomImageUrl;
     catFact.textContent = randomFact;
 
@@ -26,7 +16,6 @@ async function generateContent(event) {
 }
 
 async function getRandomCatImageUrl(){
-    
     try {
         const resp = await fetch('https://api.thecatapi.com/v1/images/search')
         const data = await resp.json();
@@ -48,20 +37,4 @@ async function getRandomCatFact(){
     } catch(err){
         console.log(err);
     }
-}
-
-function createElement(element, src, id, ...classes) {
-    const el = document.createElement(element);
-
-    if (src) {
-        el.src = src;
-    }
-
-    if(id) {
-        el.id = id;
-    }
-
-    classes.forEach(cl => el.classList.add(cl));
-
-    return el;
 }
